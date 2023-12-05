215cdbff5b6422e7cf063a5cff42e41aa09c851d
Luis Carlos Marín Campos <luiscarlosmarca@gmail.com>
2023-12-04 22:34:39 -0500

    data test drive

---
+++ s4_data_test_drive/src/test/java/DataDriveTestCSV.java
@@ -0,0 +1,73 @@
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pages.WebFormPage.WebFormPage;
+import pages.homePage.HomePage;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class DataDriveTestCSV {
+    private WebDriver driver;
+    private HomePage homePage;
+    private WebFormPage addInfo;
+
+    // Indicar la ubicación del archivo csv
+    String filePath = Paths.get("src", "test", "resources", "user.csv").toAbsolutePath().toString();
+    private CSVReader csvReader;
+    // Declaración de una variable para leer los datos del csv
+    String[] csvCell;
+
+    @BeforeTest
+    public void beforeTest() throws InterruptedException {
+         WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://qaxpert.com/webform-lab/");
+    }
+
+    @Test
+    public void agregar_informacion_web_form() throws IOException, CsvValidationException, InterruptedException {
+
+
+        homePage = new HomePage(driver);
+        // Validamos que el boton de agendar asesoria este disponible
+        if (homePage.isTitle()) {
+            // Clck en boton de agendar asesoria
+            try {
+                homePage.clickButton();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        addInfo = new WebFormPage(driver);
+
+        // Creación del objeto de tipo CSVReader
+        csvReader = new CSVReader(new FileReader(filePath));
+
+        // uso de un loop para la lectura de todos los datos del csv
+        while ((csvCell = csvReader.readNext()) != null) {
+
+            addInfo.fillName(csvCell[0]);
+            addInfo.fillEmail(csvCell[1]);
+            addInfo.fillEdad(csvCell[2]);
+            addInfo.EnviarInformacion();
+            Thread.sleep(2000);
+        }
+
+    }
+
+    @AfterTest
+    public void afterTest() {
+        driver.close();
+    }
+
+}
---
+++ s4_data_test_drive/src/test/java/DataDriveTestCSVParameters.java
@@ -0,0 +1,67 @@
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pages.WebFormPage.WebFormPage;
+import pages.homePage.HomePage;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class DataDriveTestCSVParameters {
+    private WebDriver driver;
+    private HomePage homePage;
+    private WebFormPage addInfo;
+
+
+    @BeforeTest
+    public void beforeTest() throws InterruptedException {
+         WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://qaxpert.com/webform-lab/");
+    }
+
+    @Test
+    @Parameters({"name", "email", "edad"})
+    public void agregar_informacion_web_form(String name, String email, String edad) throws IOException, CsvValidationException, InterruptedException {
+
+
+        homePage = new HomePage(driver);
+        // Validamos que el boton de agendar asesoria este disponible
+        if (homePage.isTitle()) {
+            // Clck en boton de agendar asesoria
+            try {
+                homePage.clickButton();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        addInfo = new WebFormPage(driver);
+
+        // Creación del objeto de tipo CSVReader
+
+
+        addInfo.fillName(name);
+        addInfo.fillEmail(email);
+        addInfo.fillEdad(edad);
+        addInfo.EnviarInformacion();
+        Thread.sleep(2000);
+
+
+    }
+
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
+    }
+
+}
