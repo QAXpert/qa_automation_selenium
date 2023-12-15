7cbc360c7fe2f999841827714d9e1b66b0ac262b
FedeBazan <juliopablofedericobazan@gmail.com>
2023-12-14 21:39:45 -0300

    Update Home.java

--- Fede_bazan/Workshop/workshop02_pruebatecnica/src/main/java/shop/polymerproject/org/Home.java
+++ Fede_bazan/Workshop/workshop02_pruebatecnica/src/main/java/shop/polymerproject/org/Home.java
@@ -2,9 +2,7 @@ package shop.polymerproject.org;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -22,7 +20,26 @@ public class Home {
     }
 
     public void clickOnASection() {
-        WebElement elemento = driver.findElement(By.cssSelector("a[href='/list/mens_outerwear']"));
-        elemento.click();
+        WebElement bloque1=driver.findElement(By.cssSelector("shop-app[page=\"list\"]"));
+        SearchContext context = bloque1.getShadowRoot();
+
+        WebElement bloque2= (WebElement) context.findElements(By.cssSelector("app-header#header"));
+        SearchContext context1 = bloque2.getShadowRoot();
+
+        WebElement bloque3= context1.findElement(By.cssSelector("shop-tabs[attr-for-selected=\"name\"]"));
+        SearchContext context2 = bloque3.getShadowRoot();
+
+        WebElement bloque4=context2.findElement(By.cssSelector("shop-tabs[attr-for-selected=\"name\"] shop-tab:first-child\n"));
+        SearchContext context3 = bloque4.getShadowRoot();
+
+        WebElement bloque5=context3.findElement(By.cssSelector("shop-tabs[attr-for-selected=\"name\"] shop-tab.iron-selected\n"));
+        SearchContext context4 = bloque5.getShadowRoot();
+
+        driver.findElement(mensOuterwearCssSelector).click();
+
+    }
+
+    public void attempt2clickOnASection(){
+
     }
 }
